ab6a156870d6b0ff981b0be42d17b0fa001afb05
angel <angel@prueba.com>
2024-02-07 10:42:53 +0100

    0702

---
+++ Segundo Trimestre/Modulos.js
@@ -0,0 +1,5 @@
+import {cadenaMayuscula} from "./funciones.js";
+const saludo = cadenaMayuscula("hola, bienvenido!");
+console.log(saludo);
+import { NUMEROCERO, NUMEROPI, NUMEROE} from "./funciones.js";
+console.log(NUMEROCERO, NUMEROE, NUMEROPI);
